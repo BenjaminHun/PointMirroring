c77607170623a6e77663728bde9308ce62d10a86
BenjaminHun <BenjaminHun@.github.com>
2023-12-21 00:16:47 +0100

    migrate networks to new file

--- model.py
+++ model.py
@@ -11,6 +11,7 @@ from torch.utils.data import DataLoader
 import numpy as np
 import swin_transformer as sw
 import math as m
+import nnclass as n
 device = (
     "cuda"
     if torch.cuda.is_available()
@@ -52,110 +53,6 @@ def normalize(x):
     """
     return np.array((x - np.min(x)) / (np.max(x) - np.min(x)))
 
-class ConvNetwork(nn.Module):
-    def __init__(self):
-        super().__init__()
-        self.convStack = nn.Sequential(
-            nn.Conv2d(3, 6, 3, 2, 1),
-            nn.ReLU(),
-            nn.Conv2d(6, 12, 3, 2, 1),
-            nn.ReLU(),
-            nn.Conv2d(12, 24, 3, 2, 1),
-            nn.ReLU(),
-            nn.Conv2d(24, 48, 3, 2, 1),
-            nn.ReLU(),
-            nn.Conv2d(48, 48, 3, 2, 1),
-            nn.ReLU(),
-           
-            nn.ConvTranspose2d(48, 48, 3, padding=1,
-                               output_padding=1, stride=2),
-            nn.ReLU(),
-            nn.ConvTranspose2d(48, 24, 3, padding=1,
-                               output_padding=1, stride=2),
-            nn.ReLU(),
-            nn.ConvTranspose2d(24, 12, 3, padding=1,
-                               output_padding=1, stride=2),
-            nn.ReLU(),
-            nn.ConvTranspose2d(12, 6, 3, padding=1,
-                               output_padding=1, stride=2),
-            nn.ReLU(),
-            nn.ConvTranspose2d(6, 3, 3, padding=1, output_padding=1, stride=2),
-        )
-
-    def forward(self, x):
-        input = self.convStack(x)
-        return input
-    
-class NeuralNetworkWithSwinT(nn.Module):
-    def __init__(self):
-        super().__init__()
-        self.swinT = sw.SwinTransformer(img_size=(
-            128, 128), patch_size=4, window_size=4, in_chans=3, depths=[12], embed_dim=96, drop_rate=0.3)
-        self.convStack = nn.Sequential(
-            nn.ConvTranspose2d(96, 48, 3, padding=1,
-                               output_padding=1, stride=2),
-            nn.ReLU(),
-            nn.ConvTranspose2d(48, 24, 3, padding=1,
-                               output_padding=1, stride=2),
-            nn.ReLU(),
-            nn.Conv2d(24, 12, 3, padding=1),
-            nn.ReLU(),
-            nn.Conv2d(12, 3, 3, padding=1),
-        )
-
-    def forward(self, x):
-        x = self.swinT(x)
-        H = 128
-        W = 128
-        B, HW, C = x.shape
-        newH = int((H/(m.pow((H*W)/HW, 0.5))))
-        newW = int((W/(m.pow((H*W)/HW, 0.5))))
-        x = x.view(B, C, newH, newW)
-        x = self.convStack(x)
-        return x
-
-
-class NeuralNetworkWithSwinT2(nn.Module):
-    def __init__(self):
-        super().__init__()
-        self.swinT = sw.SwinTransformer(img_size=(
-            128, 128), patch_size=4, window_size=4, in_chans=3, depths=[8], embed_dim=96, drop_rate=0.3)
-        self.swinT2 = sw.SwinTransformer(img_size=(
-            32, 32), patch_size=4, window_size=4, in_chans=96, depths=[8], embed_dim=96, drop_rate=0.3)
-
-        self.convStack = nn.Sequential(
-            nn.ConvTranspose2d(96, 48, 3, padding=1,
-                               output_padding=1, stride=2),
-            nn.ReLU(),
-            nn.ConvTranspose2d(48, 24, 3, padding=1,
-                               output_padding=1, stride=2),
-            nn.ReLU(),
-            nn.ConvTranspose2d(24, 12, 3, padding=1,
-                               output_padding=1, stride=2),
-            nn.ReLU(),
-            nn.ConvTranspose2d(12, 3, 3, padding=1,
-                               output_padding=1, stride=2),
-        )
-
-    def forward(self, x):
-        x = self.swinT(x)
-        H = 128
-        W = 128
-        B, HW, C = x.shape
-        newH = int((H/(m.pow((H*W)/HW, 0.5))))
-        newW = int((W/(m.pow((H*W)/HW, 0.5))))
-        x = x.view(B, C, newH, newW)
-        x = self.swinT2(x)
-        H = 32
-        W = 32
-        B, HW, C = x.shape
-        newH = int((H/(m.pow((H*W)/HW, 0.5))))
-        newW = int((W/(m.pow((H*W)/HW, 0.5))))
-        x = x.view(B, C, newH, newW)
-        x = self.convStack(x)
-        return x
-
-
 def trainLoop(dataloader, model, loss_fn, optimizer):
     size = len(dataloader.dataset)
     model.train()
@@ -166,7 +63,7 @@ def trainLoop(dataloader, model, loss_fn, optimizer):
         loss.backward()
         optimizer.step()
         optimizer.zero_grad()
-        if batch % 5 == 0:
+        if batch % 10 == 0:
             loss, current = loss.item(), (batch+1)*len(x)
             with open(outputPath +"/"+"results.txt","a",encoding="utf-8") as f:
                 f.write(f"loss: {loss:>7f} [{current:>5d}/{size:>5d}]\n")
@@ -216,9 +113,9 @@ print(f"Feature batch shape: {trainFeatures.size()}")
 print(f"Labels batch shape: {trainLabels.size()}")
 
 print(f"Using {device} device")
-#model=ConvNetwork()
-#model=NeuralNetworkWithSwinT()
-model = NeuralNetworkWithSwinT()
+#model=n.ConvNetwork()
+#model=n.NeuralNetworkWithSwinT()
+model = n.NeuralNetworkWithSwinT()
 
 
 if device == "cuda":
@@ -228,10 +125,11 @@ learningRate = 1e-3
 batchSize = 10
 epochs = 300
 loss = nn.MSELoss()
-optimizer = torch.optim.Adam(model.parameters(), lr=0.005)
+optimizer = torch.optim.Adam(model.parameters(), lr=0.001)
 
 for t in range(epochs):
     with open(outputPath +"/"+"results.txt","a",encoding="utf-8") as f:
         f.write(f"Epoch {t+1}\n------------------\n")
     trainLoop(trainDataloader, model, loss, optimizer)
-    testLoop(testDataloader, model, loss)
+    if t % 10:
+        testLoop(testDataloader, model, loss)
---
+++ nnclass.py
@@ -0,0 +1,106 @@
+import torch.nn as nn
+from torchvision.io import read_image
+import swin_transformer as sw
+import math as m
+
+class NeuralNetworkWithSwinT2(nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.swinT = sw.SwinTransformer(img_size=(
+            128, 128), patch_size=4, window_size=4, in_chans=3, depths=[8], embed_dim=96, drop_rate=0.3)
+        self.swinT2 = sw.SwinTransformer(img_size=(
+            32, 32), patch_size=4, window_size=4, in_chans=96, depths=[8], embed_dim=96, drop_rate=0.3)
+
+        self.convStack = nn.Sequential(
+            nn.ConvTranspose2d(96, 48, 3, padding=1,
+                               output_padding=1, stride=2),
+            nn.ReLU(),
+            nn.ConvTranspose2d(48, 24, 3, padding=1,
+                               output_padding=1, stride=2),
+            nn.ReLU(),
+            nn.ConvTranspose2d(24, 12, 3, padding=1,
+                               output_padding=1, stride=2),
+            nn.ReLU(),
+            nn.ConvTranspose2d(12, 3, 3, padding=1,
+                               output_padding=1, stride=2),
+        )
+
+    def forward(self, x):
+        x = self.swinT(x)
+        H = 128
+        W = 128
+        B, HW, C = x.shape
+        newH = int((H/(m.pow((H*W)/HW, 0.5))))
+        newW = int((W/(m.pow((H*W)/HW, 0.5))))
+        x = x.view(B, C, newH, newW)
+        x = self.swinT2(x)
+        H = 32
+        W = 32
+        B, HW, C = x.shape
+        newH = int((H/(m.pow((H*W)/HW, 0.5))))
+        newW = int((W/(m.pow((H*W)/HW, 0.5))))
+        x = x.view(B, C, newH, newW)
+        x = self.convStack(x)
+        return x
+    
+class ConvNetwork(nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.convStack = nn.Sequential(
+            nn.Conv2d(3, 6, 3, 2, 1),
+            nn.ReLU(),
+            nn.Conv2d(6, 12, 3, 2, 1),
+            nn.ReLU(),
+            nn.Conv2d(12, 24, 3, 2, 1),
+            nn.ReLU(),
+            nn.Conv2d(24, 48, 3, 2, 1),
+            nn.ReLU(),
+            nn.Conv2d(48, 48, 3, 2, 1),
+            nn.ReLU(),
+           
+            nn.ConvTranspose2d(48, 48, 3, padding=1,
+                               output_padding=1, stride=2),
+            nn.ReLU(),
+            nn.ConvTranspose2d(48, 24, 3, padding=1,
+                               output_padding=1, stride=2),
+            nn.ReLU(),
+            nn.ConvTranspose2d(24, 12, 3, padding=1,
+                               output_padding=1, stride=2),
+            nn.ReLU(),
+            nn.ConvTranspose2d(12, 6, 3, padding=1,
+                               output_padding=1, stride=2),
+            nn.ReLU(),
+            nn.ConvTranspose2d(6, 3, 3, padding=1, output_padding=1, stride=2),
+        )
+
+    def forward(self, x):
+        input = self.convStack(x)
+        return input
+    
+class NeuralNetworkWithSwinT(nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.swinT = sw.SwinTransformer(img_size=(
+            128, 128), patch_size=2, window_size=4, in_chans=3, depths=[12], embed_dim=96, drop_rate=0.3)
+        self.convStack = nn.Sequential(
+            nn.ConvTranspose2d(96, 48, 3, padding=1,
+                               output_padding=1, stride=2),
+            nn.ReLU(),
+            nn.Conv2d(48, 24, 3, padding=1,
+                            ),
+            nn.ReLU(),
+            nn.Conv2d(24, 12, 3, padding=1),
+            nn.ReLU(),
+            nn.Conv2d(12, 3, 3, padding=1),
+        )
+
+    def forward(self, x):
+        x = self.swinT(x)
+        H = 128
+        W = 128
+        B, HW, C = x.shape
+        newH = int((H/(m.pow((H*W)/HW, 0.5))))
+        newW = int((W/(m.pow((H*W)/HW, 0.5))))
+        x = x.view(B, C, newH, newW)
+        x = self.convStack(x)
+        return x
